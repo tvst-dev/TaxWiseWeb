84b540ee3e21b5f2f16b1fd54350a8b011522d69
Talevest DEv <tvstdev@gmail.com>
2025-12-29 02:11:36 +0100

    Add PaymentCallback route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import TermsOfService from "./pages/TermsOfService";
 import NotFound from "./pages/NotFound";
 import SubscriptionPage from "./pages/SubscriptionPage";
 import ResetPassword from './pages/ResetPassword';
+import PaymentCallback from './pages/PaymentCallback';
 
 const queryClient = new QueryClient();
 
@@ -34,6 +35,7 @@ const App = () => (
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/terms-of-service" element={<TermsOfService />} />
             <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/payment-callback" element={<PaymentCallback />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
